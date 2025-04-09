294c5d26b0ecbe9cd8b80f5ea728da9850089265
Mike Carr <mcarr67@gmail.com>
2025-04-09 10:17:12 -0700

    set to lowercase

--- OpenIPC_Config/ViewModels/CameraSettingsTabViewModel.cs
+++ OpenIPC_Config/ViewModels/CameraSettingsTabViewModel.cs
@@ -182,7 +182,7 @@ public partial class CameraSettingsTabViewModel : ViewModelBase
     partial void OnSelectedFpvEnabledChanged(string value)
     {
         Logger.Debug($"SelectedFpvEnabledChanged updated to {value}");
-        UpdateYamlConfig(Majestic.FpvEnabled, value);
+        UpdateYamlConfig(Majestic.FpvEnabled, value.ToLower());
     }
 
     partial void OnIsOnboardRecOnChanged(bool value)
